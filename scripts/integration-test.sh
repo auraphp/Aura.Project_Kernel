a9aa86b441ab1189a99723e892017e5ceaa6a016
Paul M. Jones <pmjones88@gmail.com>
2013-12-19 19:46:04 -0600

    add local integration test script

---
+++ scripts/integration-test.sh
@@ -0,0 +1,20 @@
+if [ -d cli-project ]
+then
+    cd cli-project
+    composer update
+    cd ..
+else
+    composer create-project aura/cli-project --prefer-dist --stability=dev
+fi
+
+rm -rf cli-project/vendor/aura/project-kernel/*
+cp -r autoload.php  cli-project/vendor/aura/project-kernel/
+cp -r composer.json cli-project/vendor/aura/project-kernel/
+cp -r config        cli-project/vendor/aura/project-kernel/
+cp -r README.md     cli-project/vendor/aura/project-kernel/
+cp -r scripts       cli-project/vendor/aura/project-kernel/
+cp -r src           cli-project/vendor/aura/project-kernel/
+cp -r tests         cli-project/vendor/aura/project-kernel/
+cd cli-project/vendor/aura/project-kernel/tests
+phpunit
+cd ../../../..
